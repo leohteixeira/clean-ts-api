8b7e4dc3d2dd5c1381f248b86908f2636dafd53b
DevLeuzera <le.tex@hotmail.com>
2022-01-17 23:23:46 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- tests/infra/criptography/bcrypt-adapter.spec.ts
+++ tests/infra/criptography/bcrypt-adapter.spec.ts
@@ -8,6 +8,10 @@ jest.mock('bcrypt', () => ({
   }
 }))
 
+export const throwError = (): never => {
+  throw new Error('mock')
+}
+
 const salt = 12
 const makeSut = (): BcryptAdapter => {
   return new BcryptAdapter(salt)
@@ -25,5 +29,12 @@ describe('Bcrypt Adapter', () => {
     const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
+  })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(throwError)
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
