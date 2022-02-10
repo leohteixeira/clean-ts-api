9cd7e486d9fee80c66d192c79d554080d7750ddf
DevLeuzera <le.tex@hotmail.com>
2022-02-10 10:35:25 -0300

    test: ensure BcryptAdapter returns true when compare succeeds

--- tests/infra/criptography/bcrypt-adapter.spec.ts
+++ tests/infra/criptography/bcrypt-adapter.spec.ts
@@ -47,4 +47,10 @@ describe('Bcrypt Adapter', () => {
     await sut.compare({ value: 'any_value', hash: 'any_hash' })
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
+
+  test('Should return true when compare succeeds', async () => {
+    const sut = makeSut()
+    const isValid = await sut.compare({ value: 'any_value', hash: 'any_hash' })
+    expect(isValid).toBe(true)
+  })
 })
