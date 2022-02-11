a8cdaa19bbd7a0fa5ac66d6eb99a8cc5abd21975
DevLeuzera <le.tex@hotmail.com>
2022-02-11 15:39:30 -0300

    test: add login route

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -1,7 +1,11 @@
 import { MongoHelper } from '@/infra/database/mongodb/helper'
 import { buildApp } from '@/main/config'
 
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 import request from 'supertest'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -13,7 +17,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -27,6 +31,25 @@ describe('Login Routes', () => {
           email: 'any_email@mail.com',
           password: 'any_password',
           passwordConfirmation: 'any_password'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on success', async () => {
+      const password = await hash('any_password', 12)
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: password
+      })
+      const app = await buildApp()
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@mail.com',
+          password: 'any_password'
         })
         .expect(200)
     })
