20a21e01b9dbf9d67d9012788844b0e07ec2115f
DevLeuzera <le.tex@hotmail.com>
2022-02-11 15:39:13 -0300

    feat: add login route

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { adaptRoute } from '@/main/adapters'
-import { makeSignUpController } from '@/main/factories/controllers'
+import { makeSignUpController, makeLoginController } from '@/main/factories/controllers'
 
 import { Router } from 'express'
 
 export const applyLoginRoutes = (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
